e42cba9f2fb050a72d88287701732fd3bcc99a3c
Hammadh Arquil <hammadhy11@gmail.com>
2023-02-15 02:00:53 +0530

    color constants

--- implementation/vitalrpm/lib/const/color_const.dart
+++ implementation/vitalrpm/lib/const/color_const.dart
@@ -1,38 +1,9 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static Color bgBlack = Color(0xff131313);
-  static Color bgBlue = Color(0xff00B5E3);
-  static Color bgGrey = Color(0xffF6F6F6);
-  static Color bgWhite = Color(0xffFFFFFF);
-  static Color buttonBlack = Color(0xff131313);
-  static Color buttonBlue = Color(0xff00B5E3);
-  static Color buttonGrey = Color(0xffF5F5F5);
-  static Color buttonOrange = Color(0xffF9A826);
-  static Color buttonPurple = Color(0xff9D6EE8);
-  static Color buttonRed = Color(0xffEB3052);
-  static Color buttonSelected = Color(0xffFBFBFB);
-  static Color cancelRed = Color(0xffCD3B45);
-  static Color cashGreen = Color(0xff90CA15);
-  static Color closedRed = Color(0xffE05050);
-  static Color completedOrderStatus = Color(0xff90CA15);
-  static Color creditBlue = Color(0xff72B7F6);
-  static Color dividerGrey = Color(0xffececec);
-  static Color inputBG = Color(0xffF6F6F6);
-  static Color inputBorderGrey = Color(0xffE4E4E4);
-  static Color notCompletedOrderStatus = Color(0xffF6F6F6);
-  static Color openGreen = Color(0xff90CA15);
-  static Color searchBG = Color(0xffF6F6F6);
-  static Color statusCancelled = Color(0xffCD3B45);
-  static Color statusDelivered = Color(0xff90CA15);
-  static Color statusNew = Color(0xff131313);
-  static Color statusPicked = Color(0xff9D6EE8);
-  static Color statusReady = Color(0xff00B5E3);
-  static Color statusStarted = Color(0xffF9A826);
-  static Color textBlack = Color(0xff303030);
-  static Color textBlue = Color(0xff00B5E3);
-  static Color textDarkGrey = Color(0xff757575);
-  static Color textGrey = Color(0xffABABAB);
-  static Color textOrange = Color(0xffF9A826);
-  static Color textWhite = Color(0xffFFFFFF);
+  static Color blue = const Color(0xFF31B5FF);
+  static Color darkBlue = const Color(0xFF070F1E);
+  static Color grey = const Color(0xFFF6F6F6);
+  static Color textgrey = const Color(0xFF939AA2);
+  static Color textblack = const Color(0xFF201F21);
 }
