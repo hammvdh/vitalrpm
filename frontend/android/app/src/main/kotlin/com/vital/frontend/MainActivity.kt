8aef53666dba1b355b2be7159a1adb62b5fc5919
Hammadh Arquil <hammadhy11@gmail.com>
2023-02-14 21:51:50 +0530

    Frontend Launch Issue Resolved.

--- frontend/android/app/src/main/kotlin/com/vital/frontend/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.vital.frontend
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ implementation/vitalrpm/android/app/src/main/kotlin/com/example/vitalrpm/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.vitalrpm
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
