a6f524cc14d39c7cbbeef02afb926746e9162c8f
Hammadh Arquil <hammadhy11@gmail.com>
2023-02-21 17:26:10 +0530

    Doctor Model

---
+++ implementation/vitalrpm/lib/models/doctor_model.dart
@@ -0,0 +1,13 @@
+class Doctor {
+  int? doctorId;
+  String? name;
+  String? speciality;
+  int? facilityId;
+  String? facilityDescription;
+  String? facilityMobileNo;
+  String? address;
+  String? image;
+  String? qualification;
+  String? about;
+  String? experience;
+}
