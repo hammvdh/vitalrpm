69e113802ac99241296516221da549e8d3ac1729
Hammadh Arquil <hammadhy11@gmail.com>
2023-02-26 22:45:45 +0530

    loading overlay

---
+++ implementation/vitalrpm/lib/widgets/loading_overlay.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:vitalrpm/const/color_const.dart';
+
+class LoadingOverlay {
+  LoadingOverlay._create(this._context);
+
+  factory LoadingOverlay.of(BuildContext context) {
+    return LoadingOverlay._create(context);
+  }
+
+  BuildContext _context;
+
+  void hide() {
+    Navigator.of(_context).pop();
+  }
+
+  void show() {
+    showDialog(
+      barrierDismissible: false,
+      context: _context,
+      useSafeArea: false,
+      builder: (BuildContext context) {
+        return _FullScreenLoader();
+      },
+    );
+  }
+
+  Future<T> during<T>(Future<T> future) {
+    show();
+    return future.whenComplete(() => hide());
+  }
+}
+
+class _FullScreenLoader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 250,
+      decoration: BoxDecoration(
+        color: AppColors.darkBlue.withOpacity(0.2),
+      ),
+      child: Center(
+        child: CircularProgressIndicator(
+          color: AppColors.textwhite,
+        ),
+      ),
+    );
+  }
+}
