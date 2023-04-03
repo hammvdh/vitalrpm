fe827bacee55e9053a69542f5a44a8c1702fe96f
Hammadh Arquil <hammadhy11@gmail.com>
2023-04-03 20:08:55 +0530

    AssessmentHistoryScreen

---
+++ implementation/vitalrpm/lib/screens/assessments/assessment_history_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AssessmentHistoryScreen extends StatefulWidget {
+  const AssessmentHistoryScreen({Key? key}) : super(key: key);
+
+  @override
+  State<AssessmentHistoryScreen> createState() =>
+      _AssessmentHistoryScreenState();
+}
+
+class _AssessmentHistoryScreenState extends State<AssessmentHistoryScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
