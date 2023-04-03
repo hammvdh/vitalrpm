1cd6147aaf9faa63ec4ef45327697a86d3fb8ae8
Hammadh Arquil <hammadhy11@gmail.com>
2023-04-03 20:09:16 +0530

    userType

--- implementation/vitalrpm/lib/providers/user_provider.dart
+++ implementation/vitalrpm/lib/providers/user_provider.dart
@@ -1,30 +1,36 @@
 // ignore_for_file: use_build_context_synchronously
 
+// import 'package:uuid/uuid.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:vitalrpm/models/user_model.dart';
 
 class UserProvider extends ChangeNotifier {
-  late UserModel loginUser;
+  UserModel loginUser = UserModel();
   late final FirebaseAuth firebaseAuth;
 
   void initialize(userId, BuildContext context) async {
     await loadUser(userId, context);
   }
 
-  Future<void> createUser(userId, email, firstname, lastname) async {
+  Future<void> createUser(userId, email, firstname, lastname, userType) async {
     DocumentReference userMasterDocument;
     FirebaseFirestore.instance.runTransaction((transaction) async {
       try {
+        // // Generate a unique user ID
+        // final uuid = Uuid();
+        // final userId = uuid.v4();
+
         loginUser = UserModel();
         userMasterDocument =
             FirebaseFirestore.instance.collection('usermaster').doc();
         loginUser.documentId = userMasterDocument.id;
-        loginUser.userId = userId!;
+        loginUser.userId = userId;
         loginUser.email = email!;
         loginUser.firstName = firstname!;
         loginUser.lastName = lastname!;
+        loginUser.userType = userType!;
         transaction.set(userMasterDocument, {
           'docId': loginUser.documentId,
           'userId': loginUser.userId,
@@ -71,13 +77,13 @@ class UserProvider extends ChangeNotifier {
     loginUser = UserModel();
     loginUser.userId = userID;
     loginUser.documentId = userDoc.get('docId');
-    loginUser.email = userDoc.get('email');
+    loginUser.email = userDoc.get('emailAddress');
     loginUser.firstName = userDoc.get('firstName');
     loginUser.lastName = userDoc.get('lastName');
     loginUser.address = userDoc.get('address');
     loginUser.country = userDoc.get('country');
     loginUser.mobileNo = userDoc.get('mobileNo');
-    loginUser.userType = userDoc.get('userType');
+    loginUser.userType = userDoc.get('usertype');
     print('User Logged In - ${loginUser.userType.trim()}');
     notifyListeners();
   }
