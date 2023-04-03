fc49fe59d6e5ab58cbdcec617d16cd5ea4c3cb3a
Hammadh Arquil <hammadhy11@gmail.com>
2023-04-03 20:09:01 +0530

    ViewAssessmentsScreen

---
+++ implementation/vitalrpm/lib/screens/assessments/view_assessment_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ViewAssessmentsScreen extends StatefulWidget {
+  const ViewAssessmentsScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ViewAssessmentsScreen> createState() => _ViewAssessmentsScreenState();
+}
+
+class _ViewAssessmentsScreenState extends State<ViewAssessmentsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
