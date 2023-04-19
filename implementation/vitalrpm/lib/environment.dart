d2fba0dd52e0898e6b17c77b8384caab3c823de5
Hammadh Arquil <hammadhy11@gmail.com>
2023-04-20 01:31:17 +0530

    Added Heroku API host

--- implementation/vitalrpm/lib/environment.dart
+++ implementation/vitalrpm/lib/environment.dart
@@ -1,6 +1,7 @@
 class Environment {
   // static const host = 'http://10.0.2.2:5000/'; // Emulator on Local
   // static const host = 'http://192.168.1.82:5000/'; // Ethernet
-  static const host = 'http://192.168.1.86:5000/'; // Wifi
+  // static const host = 'http://192.168.1.86:5000/'; // Wifi
+  static const host = 'https://vitalrpm-api.herokuapp.com/';
   // static const host = '';
 }
